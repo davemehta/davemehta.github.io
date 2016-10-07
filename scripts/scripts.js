939f0f0c1e2acb39c92e4f0661f138d9249f860c
davemehta <davemehta@rocketmail.com>
2016-10-07 14:24:55 -0700

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,10 @@
-var today = new Data();
+var today = new Date();
 var hourNow = today.getHours();
 var greeting;
 
 if (hourNow > 18) {
     greeting = 'Good Evening!;
-      } else if (hourNow >12) {
+      } else if (hourNow > 12) {
      greeting = ' Good afternoon ';
       } else if (hourNow> 0) {
        greeting = ' Good morning!' ;
